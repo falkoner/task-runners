9345227ec642de217ca79234b00ad09fed690e0a
Alex Filatau <filatau@gmail.com>
2015-08-23 21:33:04 -0700

    Added css minification

--- gulp_example/gulpfile.js
+++ gulp_example/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
   uglify = require('gulp-uglify'),
+  minifyCss = require('gulp-minify-css'),
   rename = require('gulp-rename');
 
 gulp.task('scripts', function(){
@@ -9,4 +10,11 @@ gulp.task('scripts', function(){
     .pipe(gulp.dest('js/'));
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('styles', function(){
+  gulp.src('css/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(rename('app.min.css'))
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('default', ['scripts', 'styles']);
